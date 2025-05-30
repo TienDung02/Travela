a51be3b01be2ffb83ba71ca5cecdc5874004dfdc
Nông Tiến Dũng <101783193+TienDung02@users.noreply.github.com>
2025-05-30 21:33:51 +0700

    feat(database): update migrations

--- database/migrations/2025_02_28_092333_create_itinerary_details_table.php
+++ database/migrations/2025_02_28_092333_create_itinerary_details_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('itinerary_details');
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         Schema::create('itinerary_details', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained('bookings')->onDelete('cascade');
