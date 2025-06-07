afe1c30f2d6b4fa072fd5dd329b7c0ba9ef6b43c
Nông Tiến Dũng <101783193+TienDung02@users.noreply.github.com>
2025-06-08 01:24:23 +0700

    chore(Schedule): update add calendar

--- app/Services/Map4DService.php
+++ app/Services/Map4DService.php
@@ -16,8 +16,8 @@ class Map4DService
     public function __construct()
     {
         // Lấy key và url từ file config/services.php
-        $this->apiKey = Config::get('services.map4d.key');
-        $this->baseUrl = Config::get('services.map4d.base_url');
+        $this->apiKey = env('MAP4D_API_KEY');
+        $this->baseUrl = 'https://api.map4d.vn/sdk';
 
         if (!$this->apiKey) {
             // Có thể throw exception hoặc log lỗi nếu không có API key
