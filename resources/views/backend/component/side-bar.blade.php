7a6a9cc55c593d1445ac38d6f5ea0de9bc766e76
Xuan-Hien Dieu <79910980+dieuxuanhien@users.noreply.github.com>
2025-06-13 20:54:57 +0700

    fix sidebar href bug

--- resources/views/backend/component/side-bar.blade.php
+++ resources/views/backend/component/side-bar.blade.php
@@ -11,32 +11,32 @@
                             $role = Auth::user()->role->name?? null;
                         @endphp
                         @if ($role ==='Admin')
-                        <li> <a class="waves-effect waves-dark" href="../management/admin-dashboard" aria-expanded="false"><i class="mdi mdi-gauge "></i><span class="hide-menu text-wrap">Dashboard</span></a>
+                        <li> <a class="waves-effect waves-dark" href="/management/admin-dashboard" aria-expanded="false"><i class="mdi mdi-gauge "></i><span class="hide-menu text-wrap">Dashboard</span></a>
                         </li>
                         @endif
 
                         @if($role === 'Admin')
-                            <li><a class="waves-effect waves-dark" href="../management/admin-information"><i class="mdi mdi-emoticon"></i><span class="hide-menu">Information</span></a></li>
+                            <li><a class="waves-effect waves-dark" href="/management/admin-information"><i class="mdi mdi-emoticon"></i><span class="hide-menu">Information</span></a></li>
                         @endif
                         
                         @if(in_array($role, ['Admin', 'Booking manager']))
-                            <li><a class="waves-effect waves-dark" href="../management/admin-booking"><i class="mdi mdi-account-check"></i><span class="hide-menu text-wrap">Booking</span></a></li>
+                            <li><a class="waves-effect waves-dark" href="/management/admin-booking"><i class="mdi mdi-account-check"></i><span class="hide-menu text-wrap">Booking</span></a></li>
                         @endif
 
                         @if(in_array($role, ['Admin', 'Statistics and reporting manager']))
-                            <li><a class="waves-effect waves-dark" href="../management/admin-statistic"><i class="mdi mdi-table"></i><span class="hide-menu text-wrap">Statistics & Reporting</span></a></li>
+                            <li><a class="waves-effect waves-dark" href="/management/admin-statistic"><i class="mdi mdi-table"></i><span class="hide-menu text-wrap">Statistics & Reporting</span></a></li>
                         @endif
 
                         @if(in_array($role, ['Admin', 'Packages manager']))
-                            <li><a class="waves-effect waves-dark" href="../management/admin-package"><i class="mdi mdi-earth"></i><span class="hide-menu">Packages</span></a></li>
+                            <li><a class="waves-effect waves-dark" href="/management/admin-package"><i class="mdi mdi-earth"></i><span class="hide-menu">Packages</span></a></li>
                         @endif
 
                         @if(in_array($role, ['Admin', 'Blogs manager']))
-                            <li><a class="waves-effect waves-dark" href="../management/admin-blog"><i class="mdi mdi-book-open-variant"></i><span class="hide-menu">Blogs</span></a></li>
+                            <li><a class="waves-effect waves-dark" href="/management/admin-blog"><i class="mdi mdi-book-open-variant"></i><span class="hide-menu">Blogs</span></a></li>
                         @endif
 
                         @if(in_array($role, ['Admin', 'Contact manager']))
-                            <li><a class="waves-effect waves-dark" href="../management/admin-contact"><i class="mdi mdi-help-circle"></i><span class="hide-menu">Contact</span></a></li>
+                            <li><a class="waves-effect waves-dark" href="/management/admin-contact"><i class="mdi mdi-help-circle"></i><span class="hide-menu">Contact</span></a></li>
                         @endif
                     </ul>
     
